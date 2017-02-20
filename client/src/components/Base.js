e8520fe96c685f39881f0a336370c20b9d20daf4
Taylor McManus <trm313@gmail.com>
2017-02-19 23:07:05 -0500

    Added sidebar and header

--- client/src/components/Base.js
+++ client/src/components/Base.js
@@ -1,41 +1,85 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 import Auth from '../modules/Auth';
 
-import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
-const style = {
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+const styles = {
   margin: 5,
+  title: {
+    cursor: 'pointer'
+  }
 };
 
+function handleTouchTap() {
 
-const Base = ({ children }) => (
-  <div>
-      <div className="top-bar">
-        <div className="top-bar-left">
-          <IndexLink to="/">React App</IndexLink>
-        </div>
+}
 
-        {Auth.isUserAuthenticated() ? (
-          <div className="top-bar-right">
-            <Link to="/logout">Log out</Link>
-          </div>
-        ) : (
-          <div className="top-bar-right">
-            <Link to="/signup"><RaisedButton label="Sign Up" primary={true} style={style} /></Link>
-            <Link to="/login"><RaisedButton label="Login"  style={style} /></Link>
-          </div>
-        )}
+export default class Base extends Component {
+  constructor(props) {
+    super(props);
 
-      </div>
-    { /* child component will be rendered here */ }
-    {children}
+    this.state = {
+      settingsOpen: false
+    };
+    
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
 
-  </div>
-);
+  handleToggle() {
+    this.setState({settingsOpen: !this.state.settingsOpen});
+  }
+
+  handleClose() {
+    this.setState({settingsOpen: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title={<span style={styles.title}>React Template</span>}
+          onTitleTouchTap={handleTouchTap}
+          iconElementLeft={<IconButton><NavigationMenu onTouchTap={this.handleToggle} /></IconButton>}
+          iconElementRight=
+            {Auth.isUserAuthenticated() ? (
+            <div className="top-bar-right">
+              <Link to="/logout"><FlatButton label="Log out" style={styles} /></Link>
+            </div>
+          ) : (
+            <div className="top-bar-right">
+              <Link to="/signup"><RaisedButton label="Sign Up" primary={true} style={styles} /></Link>
+              <Link to="/login"><RaisedButton label="Login"  style={styles} /></Link>
+            </div>
+          )
+          }
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.settingsOpen}
+          onRequestChange={this.handleToggle}
+        >
+          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      { /* child component will be rendered here */ }
+      {this.props.children}
+
+    </div>
+    );
+  }
+}
+
 
 Base.propTypes = {
   children: PropTypes.object.isRequired
 };
 
-export default Base;
